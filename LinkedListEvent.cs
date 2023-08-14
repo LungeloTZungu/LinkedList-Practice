d9fff7a5a6395529585c7664487ca6c3b319faf2
LungeloTZungu <lzungu56@gmail.com>
2023-08-14 20:48:49 +0200

    Add project files.

---
+++ LinkedListEvent.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class LinkedListEvent
+    {
+        public Node? First { get; set; }
+        public void InsertFirst(int data)
+        {
+            // create the node
+            Node node = new Node();
+            // put data in new node
+            node.Data = data;
+            // Put the old node in next
+            node.Next = First;
+            // make the head the new node;
+            First = node;
+        }
+
+        public Node DeleteFirst()
+        {
+            // Assign temp var
+            Node temp = First;
+            // Assign the new head
+            First = First.Next;
+            return temp;
+        }
+        public void DisplayList()
+        {
+            Console.WriteLine("Iterating through list....");
+            Node current = First;
+            while(current != null)
+            {
+                current.DisplayNode();
+                // Iterating though a node 
+                current = current.Next;
+            }
+        }
+        public void InsertLast(int data)
+        {
+            Node current = First;
+            while(current.Next != null)
+            {
+                current = current.Next;
+
+            }
+            Node node = new Node();
+            node.Data = data;
+            current.Next = node;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedListEvent linkedList = new LinkedListEvent();
+
+            linkedList.InsertFirst(100);
+            linkedList.InsertFirst(2);
+            linkedList.InsertFirst(30);
+            linkedList.InsertFirst(44);
+            linkedList.InsertFirst(90);
+
+            linkedList.DeleteFirst();
+            linkedList.DeleteFirst();
+
+            linkedList.InsertLast(546);
+            linkedList.InsertLast(1089);
+
+            linkedList.DisplayList();
+
+
+        }
+    }
+}
